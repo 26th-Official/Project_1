62d327cde20d28e9ee35554b89b955b558d0a440
26th_Official <26thofficial.creator@gmail.com>
2022-11-28 20:14:36 +0530

    Trs

--- App.js
+++ App.js
@@ -4,11 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This looks Dope</Text>
-      
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  ); 
+  );
 }
 
 const styles = StyleSheet.create({
