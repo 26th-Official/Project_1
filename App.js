cc93744a57786a225f28bd66c1f1657779fc6430
26th-Official <you@example.com>
2022-11-28 18:53:34 +0530

    Init

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This looks Dope</Text>
+      
       <StatusBar style="auto" />
     </View>
-  );
+  ); 
 }
 
 const styles = StyleSheet.create({
